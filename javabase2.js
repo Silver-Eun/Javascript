8ea623962ca67ea71c5652ef57c1cfc522f303ab
Silver-Eun <h22sang@naver.com>
2022-03-04 22:42:21 +0900

    variable/function basics

---
+++ javabase2.js
@@ -0,0 +1,10 @@
+console.clear();
+
+var a = 1;
+var b = 2;
+
+var c = a;
+a = b;
+b = c;
+console.log(a);
+console.log(b);
---
+++ javabase3.js
@@ -0,0 +1,23 @@
+console.clear();
+
+//소스코드 = 소설
+// 변수 = 인물, 사물 등 명사
+
+hi = function(name, age){
+    console.log('이름 : '+ name + ' 나이 : '+ age);
+}
+
+hi('자바', 23);
+
+fly = function(how){
+    console.log('fly ' + how + ' for 10 seconds');
+}
+
+fly('with wing');
+fly('by rocket');
+
+a = function(){
+    alert('hi');
+}
+
+a();
