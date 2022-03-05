3782ef8ffaf30aaed3e38f41a8652359b545a637
Silver-Eun <h22sang@naver.com>
2022-03-05 23:11:18 +0900

    int, string, boolean

---
+++ javabase5.js
@@ -0,0 +1,32 @@
+console.clear();
+
+var a = 4;
+var b = 2;
+var c = 'a';
+var d = true;
+var e = false;
+var f = '1';
+
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(e));
+
+console.log(a+b);
+console.log(a+f);
+
+var g = 'kevin';
+var h = g.substr(0,3);
+var i = g.substr(-3);
+
+console.log(h);
+console.log(i);
+
+var j = 3.1456;
+
+var k = parseInt(j);
+var l = parseFloat(j);
+
+console.log(k);
+console.log(l);
