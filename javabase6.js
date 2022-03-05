4f89b5177dd29cc5b9b983f0ca410c9b37d96fd6
Silver-Eun <h22sang@naver.com>
2022-03-05 23:11:57 +0900

    setTimeour, Interval

---
+++ javabase6.js
@@ -0,0 +1,37 @@
+console.clear();
+
+/*function a(){
+    $('body').addClass('active')
+}
+
+setTimeout(a, 100);
+
+function b(){
+    $('body').toggleClass('active')
+}
+
+setInterval(b, 2000);*/
+
+var d = "\'따옴표\'\"큰따옴표\"";
+
+console.log(d);
+
+var a = 5 + 4 * 2;
+
+console.log(a);
+
+var b = 1;
+++b;
+console.log(b);
+
+var bb = 1;
+console.log(++bb);
+console.log(bb);
+
+bb = 1;
+console.log(bb++);
+console.log(bb);
+
+var random = Math.random();
+console.log(random);
+console.log(random*100);
