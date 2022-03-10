cc5eaf09091624fbe0608cb34376a2c96ec8e777
Silver-Eun <h22sang@naver.com>
2022-03-10 20:29:36 +0900

    if

---
+++ javabase10-1.js
@@ -0,0 +1,26 @@
+console.clear();
+
+function prompt2(msg){
+  return prompt(msg)
+}
+
+var a = prompt2('name');
+console.log(name);
+
+var age = prompt2('age');
+console.log(age);
+
+var age = prompt2('address');
+console.log(addres);
+
+if(name = 'kevin' && age >= 20){
+  console.log(name + ' is adult');
+}
+
+if(adress = ''){
+  console.log(name + ", please write address");
+}
+
+if(adress = LA || age > 15){
+  console.log(name + 's address is LA or oler than 15 years old');
+}
---
+++ javabase10.js
@@ -0,0 +1,28 @@
+console.clear();
+
+var age = 18;
+
+if(age >= 20){
+    console.log('adult');
+}else if(age >= 15){
+    console.log('teenager');
+}else{
+    console.log('child');
+}
+
+var child = true;
+
+if(child){
+    console.log('YES');
+}
+
+var a = 1;
+var b = "hi";
+var c = true;
+var d = false;
+
+var gen = "male";
+
+if(age > 20 || gen == "male"){
+    console.log('hi');
+}
