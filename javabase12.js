eb73406c059ba0663f110237796d6944c9362e22
Silver-Eun <h22sang@naver.com>
2022-03-13 14:20:09 +0900

    while

---
+++ javabase12.js
@@ -0,0 +1,32 @@
+console.clear();
+
+/*var dan = 8;
+var a = 1;
+
+while (a < 10){
+    console.log(dan + '*' + a + '=' + dan*a);
+
+    a++;
+}
+
+var i = -500;
+
+while(i < 500){
+    console.log(i);
+
+    i++;
+}*/
+
+function hinTime(n){
+    console.log('=== hi ' + n + 'Time1 ===')
+    var i = 1;
+    
+    while(i <= n){
+        console.log('hi#' + i);
+        i++;
+    }
+}
+
+hinTime(3);
+
+hinTime(13);
