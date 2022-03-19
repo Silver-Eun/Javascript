692823adb8ca5df85402f9bc211108e81823edcf
Silver-Eun <h22sang@naver.com>
2022-03-19 22:59:14 +0900

    DOM without JQuery2

---
+++ javabase16.js
@@ -0,0 +1,22 @@
+console.clear();
+
+/*var div = document.getElementsByTagName('div');
+
+for(var i = 0; i<= div.length; i++){
+    div[i].innerHTML = div[i].innerHTML + 'times';
+}
+
+$('div').append('번');*/
+
+/*$('div');
+
+function pack(){
+    return 1;
+}
+
+var a = pack();
+
+console.log(a);*/
+
+var pack_div = $('div');
+pack_div.css('color', 'red');
