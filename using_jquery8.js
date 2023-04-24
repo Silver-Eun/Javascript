61515f00e8c2e4ec76bfff9c24a91ffba0487cb7
Silver-Eun <h22sang@naver.com>
2023-04-24 23:50:54 +0900

    vue.js 2.0 Example1

---
+++ using_jquery8.js
@@ -0,0 +1,26 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    name: "kim",
+    age: "25",
+    title: "hi",
+    text: "text",
+  },
+});
+
+// var name = "kim";
+// var age = 25;
+// var title = "hi";
+// var body = "text";
+
+// $(".input-name").empty().append(name);
+// $(".input-age").empty().append(age);
+// $(".input-title").empty().append(title);
+// $(".input-body").empty().append(body);
+
+// function run() {
+//   $(".input-name").empty().append("Lee");
+//   $(".input-age").empty().append(age);
+//   $(".input-title").empty().append(title);
+//   $(".input-body").empty().append(body);
+// }
