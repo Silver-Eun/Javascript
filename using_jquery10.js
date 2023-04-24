db05f84e4d4477f64bed14805162dcaec9c277b2
Silver-Eun <h22sang@naver.com>
2023-04-24 23:51:24 +0900

    vue.js 2.0 Example3

---
+++ using_jquery10.js
@@ -0,0 +1,19 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    sectionStyle: {
+      "background-color": "red",
+      height: "100px",
+    },
+  },
+});
+
+var app = new Vue({
+  el: "#root",
+  data: {
+    sectionstyle: {
+      "background-color": "red",
+      height: "200px",
+    },
+  },
+});
