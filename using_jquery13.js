77758070ad002e61d3e526d0f5f27b11c81ef27d
Silver-Eun <h22sang@naver.com>
2023-04-25 17:30:21 +0900

    vue.js 2.0 Example6

---
+++ using_jquery13.js
@@ -0,0 +1,26 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    selected_num: 1,
+  },
+});
+
+// var app = new Vue({
+//   el: "#app",
+//   data: {
+//     value: 1,
+//   },
+//   methods: {
+//     selected: function (event) {
+//       this.value = event.target.value;
+//     },
+//   },
+// });
+
+// for (var i = 1; i <= 8; i++) {
+//   $("select").append('<option value="' + i + '">' + i + "</option>");
+// }
+
+// $("select").change(function () {
+//   $("#txt").text(event.target.value);
+// });
